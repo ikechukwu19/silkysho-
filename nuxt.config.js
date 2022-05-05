d1fdef3980f52b42b2c93bb480d3561ca05b3ef2
iyke prosper <bestielawhine@gmail.com>
2022-05-05 15:52:34 +0100

    nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,11 +16,12 @@ export default {
     ]
   },
 
+  buildDir: 'nuxt-dist',
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/main.css',
   ],
-
+  target: 'static',
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],

7c7efca045340016854af1ff28c4e58209ace1cb
iyke prosper <bestielawhine@gmail.com>
2022-05-05 14:36:55 +0100

    seo

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,6 +14,18 @@ export default {
       link: [
         {rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'},
         {rel: 'stylesheet', type: 'text/css', href: 'https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css'}
+      ],
+       meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Get amazing products from the best online shop'
+        },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content: 'good products, online store, best prices'
+        }
       ]
     }
   }
